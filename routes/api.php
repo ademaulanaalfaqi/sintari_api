55a3f46da692bfa3b319538cd4e9f9c92597420a
ariyanto08 <102232039+ariyanto08@users.noreply.github.com>
2023-10-09 21:41:29 +0700

    tpp total

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\SintariController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/',function(){
+    return response()->json([
+        'status'=>false,
+        'message'=>'tak boleh akses'
+    ],401);
+})->name('login');
+Route::post('registerUser',[AuthController::class,'registerUser']);
+Route::post('loginUser',[AuthController::class,'loginUser']);
+Route::get('sintari',[SintariController::class,'index'])->middleware('auth:sanctum');
+Route::get('logout',[AuthController::class,'logout']);
